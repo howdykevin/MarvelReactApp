6d654cfe7680f2b517c55d57c32b884fcb3774e7
Kevin Laptop <dev4@bankedge.com>
2019-01-15 17:05:33 +1000

    Tinder card UI Part 2

--- my-app/src/components/Card.js
+++ my-app/src/components/Card.js
@@ -22,11 +22,11 @@ class CardContent extends Component {
           <CardImg
             top
             width="100%"
-            src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=300"
+            src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=200"
             alt="Card image cap"
           />
           <CardBody>
-            <CardTitle>Card title</CardTitle>
+            <CardTitle>{this.props.title}</CardTitle>
             <CardSubtitle className="d-none d-md-block">
               Card subtitle
             </CardSubtitle>
@@ -34,7 +34,7 @@ class CardContent extends Component {
               Some quick example text to build on the card title and make up the
               bulk of the card's content.
             </CardText>
-            <Button>View Full Profile</Button>
+            <Button color="primary">View Full Profile</Button>
           </CardBody>
         </Card>
       </div>
--- my-app/src/components/TinderCards.js
+++ my-app/src/components/TinderCards.js
@@ -1,19 +1,104 @@
 import React, { Component } from "react";
 import { Card, CardWrapper } from "react-swipeable-cards";
 import CardContent from "./Card";
+import { Alert } from "reactstrap";
+import "./TinderCards.css";
+
+const data = [
+  { id: 1, name: "First" },
+  { id: 2, name: "Second" },
+  { id: 3, name: "third" },
+  { id: 4, name: "fourth" }
+];
 
 class MarvelCards extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      direction: "",
+      visible: true
+    };
+    this.onSwipeLeft = this.onSwipeLeft.bind(this);
+    this.onSwipeRight = this.onSwipeRight.bind(this);
+    this.removeAlert = this.removeAlert.bind(this);
+    this.onDoubleTap = this.onDoubleTap.bind(this);
+  }
+
+  removeAlert() {
+    setTimeout(() => {
+      this.setState({
+        visible: false
+      });
+    }, 2000);
+  }
+
+  onSwipeLeft(data) {
+    console.log("Swipe Left");
+    this.setState(
+      {
+        direction: "left"
+      },
+      () => {
+        this.removeAlert();
+        this.setState({ visible: true });
+      }
+    );
+  }
+
+  onSwipeRight(data) {
+    console.log("Swipe Right");
+    // this.getAlerts("Right");
+    this.setState(
+      {
+        direction: "right"
+      },
+      () => {
+        this.removeAlert();
+        this.setState({ visible: true });
+      }
+    );
+  }
+
+  onDoubleTap(data) {
+    console.log("Double Tapped");
   }
 
   render() {
-    return (
-      <CardWrapper>
-        <Card>
-          <CardContent />
+    const cardDecks = data.map(item => {
+      return (
+        <Card
+          onSwipeLeft={this.onSwipeLeft}
+          onSwipeRight={this.onSwipeRight}
+          onDoubleTap={this.onDoubleTap}
+          key={item.id}
+        >
+          <CardContent title={item.name} />
         </Card>
-      </CardWrapper>
+      );
+    });
+    return (
+      <div>
+        {/* creating alert for every swipe */}
+        {this.state.direction === "left" ? (
+          <Alert
+            className="alert-left text-center"
+            color="danger"
+            isOpen={this.state.visible}
+          >
+            Nope
+          </Alert>
+        ) : this.state.direction === "right" ? (
+          <Alert
+            className="alert-right text-center"
+            color="success"
+            isOpen={this.state.visible}
+          >
+            Nice!
+          </Alert>
+        ) : null}
+        {/* tinder cards */}
+        <CardWrapper>{cardDecks}</CardWrapper>
+      </div>
     );
   }
 }
